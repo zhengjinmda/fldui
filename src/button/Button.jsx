856d77b5150a17c65b05808e9403cc0b3201476a
zhengjinmda <903355860@qq.com>
2020-08-19 16:28:33 +0800

    init

---
+++ src/button/Button.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./button.less";
+
+class Button extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div className='content'>hello</div>;
+  }
+}
+
+export default Button;
