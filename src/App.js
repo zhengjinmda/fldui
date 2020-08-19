b56f66d55369e4dd75d5e66680573dddd8b50eab
zhengjinmda <903355860@qq.com>
2020-08-19 19:31:29 +0800

    button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import Button from './button'
+import React, { Component } from 'react';
+import Button from './button';
 
-function App() {
-  return (
-    <div>
-      <Button></Button>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>button</h1>
+          <h2>type</h2>
+          <Button>primary</Button>
+          <Button type='danger'>danger</Button>
+          <Button type='warning'>warning</Button>
+          <Button type='success'>success</Button>
+          <Button type='info'>info</Button>
+          <h2>size</h2>
+          <Button size='default'>default</Button>
+          <Button size='large'>large</Button>
+          <Button size='small'>small</Button>
+          <h2>disabled</h2>
+          <Button disabled={true}>disabled</Button>
+          <h2>circle</h2>
+          <Button circle={true}>圆</Button>
+          <Button circle={true}>Circle</Button>
+          <h2>icon</h2>
+          <Button icon="icon-search">icon</Button>
+          <Button icon="icon-search" circle={true}></Button>
+          <h2>function</h2>
+          <Button onClick={()=>{console.log(1111)}}>primary</Button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/button/Button.jsx
+++ src/button/Button.jsx
@@ -1,14 +1,50 @@
 import React, { Component } from "react";
-import "./button.less";
+import PropTypes from "prop-types";
+import classnames from "classnames";
+import "./styles/fld-button.less";
 
 class Button extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+  handleClick = () => {
+      if(!this.props.onClick) return
+      this.props.onClick() 
+  }
   render() {
-    return <div className='content'>hello</div>;
+    const btnClassName = classnames({
+      "fld-btn": true,
+      [`fld-btn_${this.props.type}`]: true,
+      [`fld-btn_${this.props.size}`]: true,
+      "fld-btn_disabled": this.props.disabled,
+      "fld-btn_circle": this.props.circle,
+    });
+    return (
+      <button className={btnClassName} onClick={this.handleClick} >
+        {this.props.icon ? (<span className={`iconfont fld-btn_icon ${this.props.icon}`} />) : null}
+        {this.props.children}
+      </button>
+    ) 
+
   }
 }
+
+Button.defaultProps = {
+  children: "Button",
+  type: "primary",
+  size: "default",
+  disabled: false,
+  circle: false,
+  icon: '',
+};
+
+Button.propTypes = {
+  children: PropTypes.string,
+  type: PropTypes.oneOf(["primary", "success", "warning", "danger", "info"]),
+  size: PropTypes.oneOf(["default", "small", "large"]),
+  disabled: PropTypes.bool,
+  circle: PropTypes.bool,
+};
 
 export default Button;
